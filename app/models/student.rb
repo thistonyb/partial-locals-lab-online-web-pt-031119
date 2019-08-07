5c0e2e146f4290e3e52a598b74ca1da736a52856
Tony Baker <this.tonybaker@gmail.com>
2019-08-07 22:04:30 +0000

    Done.

--- app/models/student.rb
+++ app/models/student.rb
@@ -13,4 +13,8 @@
 class Student < ActiveRecord::Base
   has_many :classroom_students
   has_many :classrooms, through: :classroom_students
+
+  def self.search(text)
+    Student.all.where("name like ?",'%'+text+'%')
+  end
 end

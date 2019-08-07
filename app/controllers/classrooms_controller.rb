580c73e9c3dd9a512f0eccf49d3a0d35467813d5
Tony Baker <this.tonybaker@gmail.com>
2019-08-07 23:34:47 +0000

    Automatically backed up by Learn

--- app/controllers/classrooms_controller.rb
+++ app/controllers/classrooms_controller.rb
@@ -2,6 +2,7 @@ class ClassroomsController < ApplicationController
   def show
     @classroom = Classroom.find(params[:id])
     @student = @classroom.oldest_student
+    @classrooms = Classroom.all
   end
 
   def index
